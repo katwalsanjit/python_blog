2f49f50eeb1fd1adc91e3adf4f3c4ef5a946d377
sanjit69 <sanjitkatwal@gmail.com>
2019-05-09 06:52:07 +0545

    admin and migration created

---
+++ blog_post/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+# from .import views (.) dot means same directory
+#import views
+# this is taken from root this is also called absolute or static method
+from blog_post import views
+from django.contrib import admin
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", views.home, name="home")
+]
